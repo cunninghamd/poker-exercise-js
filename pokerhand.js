c126ec5bad8e3329af78d2f6dbe3bd3dadec52a0
Derek Cunningham <cunninghamd@nosleep.ca>
2016-10-20 23:46:49 -0400

    current state

--- pokerhand.js
+++ pokerhand.js
@@ -4,6 +4,7 @@ function PokerHand(hand) {
         values: "AKQJ098765432"
     };
     var cards = [];
+    var properties = {};
     this.category = "";
     
     /*
@@ -15,7 +16,9 @@ function PokerHand(hand) {
         cards.push(hand.charAt(i) + hand.charAt(i + 1));
     }
     
-    cards = this.sortCards(cards);
+    var sortedCards = this.sortCards(cards);
+    properties = this.getProperties(sortedCards);
+    this.category = properties.category;
     
     this.getCards = function() {
         return cards;
@@ -37,6 +40,52 @@ PokerHand.prototype.sortCards = function(cards) {
     
     return cards;
 };
+
+PokerHand.prototype.getProperties(sortedCards) {
+    var weights = this.weights;
+    
+    var properties = {
+        royalFlush: true,
+        straightFlush: true,
+        fourOfAKind: true,
+        fullHouse: true,
+        flush: true,
+        straight: true,
+        threeOfAKind: true,
+        twoPair: true,
+        onePair: true,
+        
+        suitsMatch: true,
+        valuesMatch: true,
+        category: ""
+    };
+    
+    var cardValues = [];
+    
+    for (var i = 0; i < cards.length; i++) {
+        var card = cards[i];
+        var cardValueIndex = weights.values.indexOf(card.charAt[0]);
+        
+        if (i === 0 && weights.values.indexOf(card.charAt(0)) !== 0) {
+            properties.royalFlush = false;
+        }
+        
+        if (i > 0 && weights.values.indexOf(card.charAt(0)) !== weights.values.indexOf(cards[i - 1].charAt(0))) {
+            properties.straightFlush = false;
+            properties.straight = false;
+        }
+        
+        cardValues[cardValueIndex] = cardValues[cardValueIndex]++ || 1; 
+    }
+    
+    var matches = [];
+    
+    for (var i = 0; i < cardValues.length; i++) {
+        if (cardValues[i] > 1) {
+            matches.push[];
+        }
+    }
+}
 
 // is left card better than the right card
 PokerHand.prototype.betterThan = function(lc, rc) {
